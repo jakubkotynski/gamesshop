df241c1585f04072419f6cfda7af8b6f3f8bc105
dimirhouse <dimirhouse@gmail.com>
2018-03-03 18:37:01 +0100

    added appconfig and persistenceconfig class

--- games-shop-service/src/main/java/service/config/AppConfig.java
+++ games-shop-service/src/main/java/service/config/AppConfig.java
@@ -1,4 +1,23 @@
 package service.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@Import(PersistenceConfig.class)
+@ComponentScan(basePackages = {"service.config"})
 public class AppConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        propertySourcesPlaceholderConfigurer.setLocations(
+                new ClassPathResource("persistence.properties"));
+
+        return propertySourcesPlaceholderConfigurer;
+    }
 }
