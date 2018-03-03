5df3a7518e4a2082fdbbfde60ef79727e3c34852
dimirhouse <dimirhouse@gmail.com>
2018-03-03 12:12:21 +0100

    added WebConfig, WebAppInitializer and MainController in web package, added WEB-INF directory and upgraded pom.xml

---
+++ games-shop-web/src/main/java/config/WebAppInitializer.java
@@ -0,0 +1,54 @@
+package config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+
+        AnnotationConfigWebApplicationContext applicationContext = getApplicationContext();
+
+        servletContext.addListener(new ContextLoaderListener(applicationContext));
+
+        registerCustomFilters(servletContext);
+
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher",
+                new DispatcherServlet(getDispatcherContext()));
+        configureDispatcher(dispatcher);
+    }
+
+    private void registerCustomFilters(ServletContext servletContext) {
+        FilterRegistration.Dynamic encodingFilter = servletContext.addFilter("encodingFilter",
+                new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+    }
+
+    private void configureDispatcher(ServletRegistration.Dynamic dispatcher) {
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+
+    private AnnotationConfigWebApplicationContext getApplicationContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+
+        return context;
+    }
+
+    private AnnotationConfigWebApplicationContext getDispatcherContext() {
+        AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
+        dispatcherContext.register(WebConfig.class);
+
+        return dispatcherContext;
+    }
+}
